cd1e036e49151cd7ff9f95cad700e5a8de8978e2
DESKTOP-HNQLD8H\Administrator <591325435@qq.com>
2019-10-31 10:56:21 +0800

    login-pic address update

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -3,42 +3,9 @@
     <p class="login-logo">
       <img :src="logoImg" width="500" height="124" />
     </p>
-    <!-- <div class="login">
+    <div class="login">
       <h3>欢迎登录</h3>
-      <a-form ref="loginForm" :model="user" :rules="rules">
-        <FormItem prop="username">
-          <a-input
-            class="login-input"
-            id="cao"
-            size="large"
-            v-model="user.username"
-            placeholder="请输入用户名"
-            :on-blur="saveUserName"
-          >
-            <span class="icon-pre" slot="prepend">
-              <Icon type="person"></Icon>
-            </span>
-          </a-input>
-        </FormItem>
-        <FormItem prop="password">
-          <a-input
-            type="password"
-            class="login-input"
-            size="large"
-            v-model="user.password"
-            placeholder="请输入登录密码"
-            @on-enter="loginMe"
-          >
-            <span class="icon-pre" slot="prepend">
-              <Icon type="ios-locked"></Icon>
-            </span>
-          </a-input>
-        </FormItem>
-        <FormItem>
-          <Button type="primary" class="login-btn" @click="loginMe">登录</Button>
-        </FormItem>
-      </a-form>
-    </div>-->
+    </div>
     <div class="login-footer">
       <p class="login-foot">
         <img :src="icpImg" width="28" height="28" /> 京ICP备05007183号 ｜
@@ -53,9 +20,9 @@
 </template>
 
 <script>
-import logoImg from "@/assets/images/login/logo.png";
-import icpImg from "@/assets/images/login/icp@2x.png";
-import policeImg from "@/assets/images/login/police@2x.png";
+import logoImg from "assets/images/login/logo.png";
+import icpImg from "assets/images/login/icp@2x.png";
+import policeImg from "assets/images/login/police@2x.png";
 
 export default {
   data() {
